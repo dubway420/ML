4dbb29bd148813fb024528f6009fb2ab9ebd13e7
Huw <Huw@LAPTOP-8KNQKN9V>
2018-10-31 06:36:26 +0000

    test

--- test.py
+++ test.py
@@ -1,12 +1,40 @@
-from sklearn import datasets
-digits = datasets.load_digits()
+#Import Library
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn import svm
+import numpy as np
+from sklearn.metrics import accuracy_score
 
-images_and_labels = list(zip(digits.images, digits.target))
-for index, (image, label) in enumerate(images_and_labels[:4]):
-    plt.subplot(2, 4, index + 1)
-    plt.axis('off')
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Training: %i' % label)
-    
-plt.show()    
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
+
+C = 1.0 # SVM regularization parameter
+svc = svm.SVC(kernel='linear', C=10,gamma=0.1).fit(X, y)
+# create a mesh to plot in
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+h = (x_max / x_min)/100
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+ np.arange(y_min, y_max, h))
+
+plt.subplot(1, 1, 1)
+Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
+
+predictions = svc.predict(X)
+
+print("The accuracy was: " + str(round(accuracy_score(y, predictions), 2)*100) + "%")
+
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
+plt.scatter(X[:, 0], X[:, 1], c=y)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max())
+plt.title('SVC with linear kernel')
+plt.show()
